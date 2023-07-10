df57e1fd1dbb439fb8e5bbdb253f71820e8ef91d
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-10 17:21:30 -0500

    feat: :sparkles: enpoint para crear datos
    en la tabla bodegas y se comenta como debe ingresar los datos

--- routers/bodegas.js
+++ routers/bodegas.js
@@ -7,7 +7,7 @@ import con from '../server/db.js';
 const appBodegas = express.Router();
 
 /**
- *  ! Metodo GET
+ *  ! Metodo GET listar los datos de bodegas
  */
 appBodegas.get('/:id?', (req, res) => {
     (req.params.id) ?
@@ -19,11 +19,41 @@ appBodegas.get('/:id?', (req, res) => {
         )
     :
     con.query(
-        /*sql*/`SELECT * FROM bodegas`,
+        /*sql*/`SELECT * FROM bodegas ORDER BY nombre`,
         (err, data, fils) => {
             res.send(data);
         }
         );
 });
-
+/**
+ * ! metodo POST
+ * 
+ * ? manera de insertar los datos
+ */
+/*  
+    "nombre": "string",
+    "id_responsable": bigint,
+    "estado": tyninit,
+    "created_by": bigint,
+    "update_by": bigint,
+    "created_at": "timeStamp",
+    "updated_at": timeStamp,
+    "deleted_at": timeStamp
+*/
+appBodegas.post('/',(req, res) => {
+    const body = req.body
+        con.query(
+        /*sql*/`INSERT INTO bodegas SET ?`,
+        body,
+        (err,data,fils) => {
+            data.affectedRows += 200;
+            let resul = body;
+            resul.id = data.insertId;
+            res.status(201).json({
+                message : "se ha creado con exito",
+                data : resul
+            })
+        }
+    )
+})
 export default appBodegas;
